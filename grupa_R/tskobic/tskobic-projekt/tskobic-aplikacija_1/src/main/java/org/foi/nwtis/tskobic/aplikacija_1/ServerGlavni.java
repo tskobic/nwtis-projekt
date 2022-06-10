ec4d82cd8c79a44487e35f79239cb5ba49a8a56c
tskobic <tskobic@58350e7a-5921-49bc-b2bb-43b6036b2832>
2022-06-10 17:19:06 +0000

    10.6

--- grupa_R/tskobic/tskobic-projekt/tskobic-aplikacija_1/src/main/java/org/foi/nwtis/tskobic/aplikacija_1/ServerGlavni.java
+++ grupa_R/tskobic/tskobic-projekt/tskobic-aplikacija_1/src/main/java/org/foi/nwtis/tskobic/aplikacija_1/ServerGlavni.java
@@ -1,5 +1,111 @@
 package org.foi.nwtis.tskobic.aplikacija_1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.foi.nwtis.tskobic.vjezba_03.konfiguracije.Konfiguracija;
+import org.foi.nwtis.tskobic.vjezba_03.konfiguracije.KonfiguracijaApstraktna;
+import org.foi.nwtis.tskobic.vjezba_03.konfiguracije.NeispravnaKonfiguracija;
+
+/**
+ * Glavna klasa poslužitelja ServerGlavni.
+ */
 public class ServerGlavni {
 
+	/** broj porta. */
+	int port;
+
+	/** maksimalni broj čekača. */
+	int maksCekaca;
+
+	/** maksimalni broj čekanja. */
+	int maksCekanje;
+
+	/** veza. */
+	Socket veza = null;
+
+	/** konfiguracijski podaci. */
+	public Konfiguracija konfig = null;
+
+	/** iso format za datum. */
+	static SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	/**
+	 * Konstruktor klase.
+	 *
+	 * @param port       broj porta
+	 * @param maksCekaca maksimalan broj čekača
+	 */
+	public ServerGlavni(int port, int maksCekaca, int maksCekanje) {
+		super();
+		this.port = port;
+		this.maksCekaca = maksCekaca;
+		this.maksCekanje = maksCekanje;
+	}
+
+	/**
+	 * Učitavanje konfiguracijskih podataka.
+	 *
+	 * @param nazivDatoteke naziv datoteke konfiguracijskih podataka
+	 */
+	public void ucitavanjePodataka(String nazivDatoteke) {
+		try {
+			this.konfig = KonfiguracijaApstraktna.preuzmiKonfiguraciju(nazivDatoteke);
+		} catch (NeispravnaKonfiguracija e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Obrada zahtjeva.
+	 */
+	public void obradaZahtjeva() {
+
+		try (ServerSocket ss = new ServerSocket(this.port, this.maksCekaca)) {
+			ss.setSoTimeout(maksCekanje);
+			while (true) {
+				this.veza = ss.accept();
+
+				DretvaZahtjeva dretvaZahtjeva = new DretvaZahtjeva(this, konfig, veza);
+				dretvaZahtjeva.start();
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(ServerGlavni.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+
+	/**
+	 * Glavna metoda.
+	 *
+	 * @param args argumenti
+	 */
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Broj argumenata nije 1.");
+			return;
+		}
+		ServerGlavni sg = new ServerGlavni(0, 0, 0);
+
+		sg.ucitavanjePodataka(args[0]);
+
+		if (sg.konfig == null) {
+			System.out.println("Problem s konfiguracijom.");
+			return;
+		}
+
+		int port = Integer.parseInt(sg.konfig.dajPostavku("port"));
+		int maksCekaca = Integer.parseInt(sg.konfig.dajPostavku("maks.cekaca"));
+		int maksCekanje = Integer.parseInt(sg.konfig.dajPostavku("maks.cekanje"));
+
+		sg.port = port;
+		sg.maksCekaca = maksCekaca;
+		sg.maksCekanje = maksCekanje;
+
+		sg.obradaZahtjeva();
+	}
 }

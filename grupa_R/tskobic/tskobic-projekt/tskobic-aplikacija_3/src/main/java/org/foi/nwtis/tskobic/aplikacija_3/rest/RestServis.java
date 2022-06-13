5844945c0d72546b16746b9152d7e660e0897376
tskobic <tskobic@58350e7a-5921-49bc-b2bb-43b6036b2832>
2022-06-13 21:23:54 +0000

    NWTiS projekt 13.6.: - 3. app initialized

---
+++ grupa_R/tskobic/tskobic-projekt/tskobic-aplikacija_3/src/main/java/org/foi/nwtis/tskobic/aplikacija_3/rest/RestServis.java
@@ -0,0 +1,12 @@
+package org.foi.nwtis.tskobic.aplikacija_3.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Klasa RestServis koja nasljeđuje klasu jakarta.ws.rs.core.Application.
+ */
+@ApplicationPath("api")
+public class RestServis extends Application {
+
+}
